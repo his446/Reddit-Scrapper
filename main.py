3481b54265f4bc9baa455a5f95319cf7811dd811
hisoka <hisoka>
2025-07-24 17:54:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+import praw
+
+load_dotenv()
+client_id=os.getenv("CLIENT_ID", "")
+client_secret=os.getenv("CLIENT_SECRET", "")
+user_agent=os.getenv("USER_AGENT", "")
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent
+)
+
+subreddit = reddit.subreddit("python")
+for post in subreddit.hot(limit=5):
+    print(post.title)
