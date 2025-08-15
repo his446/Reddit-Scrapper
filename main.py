c8bcac417f88594d146838542d0d100aaac09556
hisoka <hisoka>
2025-08-15 17:49:32 +0200

    add NEWSAPI source

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ client_id=os.getenv("CLIENT_ID", "")
 client_secret=os.getenv("CLIENT_SECRET", "")
 user_agent=os.getenv("USER_AGENT", "")
 
+subreddits = ["news", "worldnews", "politics", "technolgy", "economics"]
+
 reddit = praw.Reddit(
     client_id=client_id,
     client_secret=client_secret,
     user_agent=user_agent
 )
+print(reddit.read_only)
+# subreddit = reddit.subreddit("news")
+# for post in subreddit.hot(limit=5):
+#     print(post.title)
 
-subreddit = reddit.subreddit("python")
-for post in subreddit.hot(limit=5):
-    print(post.title)
